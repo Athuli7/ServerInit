f2cd0617d8ce5142a141028d8d5d6ea4fa7b0c38
Athul B Raj <mail@athulraj.com>
2016-05-30 09:16:35 +0530

    debconf-set-selections fix

--- production.sh
+++ production.sh
@@ -4,11 +4,11 @@ echo "MySQL root password : "
 read MySQLrootpassword
 sudo apt-get update
 sudo apt-get install apache2 -y
-echo "mysql-server mysql-server/root_password select (password omitted)" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again select (password omitted)" | debconf-set-selections
-# Deprectiated : export DEBIAN_FRONTEND=noninteractive
+echo "mysql-server mysql-server/root_password select $MySQLrootpassword" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again select $MySQLrootpassword" | debconf-set-selections
+# Depretiated : export DEBIAN_FRONTEND=noninteractive
 sudo  apt-get install mysql-server php5-mysql -y
-mysqladmin -u root password $MySQLrootpassword
+# Depretiated : mysqladmin -u root password $MySQLrootpassword
 sudo mysql_install_db
 
 mysql -sfu root "DELETE FROM mysql.user WHERE User='';"
@@ -17,7 +17,7 @@ mysql -sfu root "DROP DATABASE test;"
 mysql -sfu root "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
 mysql -sfu root "FLUSH PRIVILEGES;"
 
-sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
 
 echo /etc/apache2/mods-enabled/dir.conf <  "<IfModule mod_dir.c>"
 echo /etc/apache2/mods-enabled/dir.conf << "<DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm"
