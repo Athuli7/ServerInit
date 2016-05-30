85df288b6216667ca99dea7466cc40ed36c185bc
Athul B Raj <mail@athulraj.com>
2016-05-30 09:53:13 +0530

    sudoed

--- production.sh
+++ production.sh
@@ -4,23 +4,23 @@ echo "MySQL root password : "
 read MySQLrootpassword
 sudo apt-get update
 sudo apt-get install apache2 -y
-echo "mysql-server mysql-server/root_password select $MySQLrootpassword" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again select $MySQLrootpassword" | debconf-set-selections
+sudo echo "mysql-server mysql-server/root_password select $MySQLrootpassword" | debconf-set-selections
+sudo echo "mysql-server mysql-server/root_password_again select $MySQLrootpassword" | debconf-set-selections
 # Depretiated : export DEBIAN_FRONTEND=noninteractive
 sudo  apt-get install mysql-server php5-mysql -y
 # Depretiated : mysqladmin -u root password $MySQLrootpassword
 sudo mysql_install_db
 
-mysql -sfu root "DELETE FROM mysql.user WHERE User='';"
-mysql -sfu root "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-mysql -sfu root "DROP DATABASE test;"
-mysql -sfu root "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
-mysql -sfu root "FLUSH PRIVILEGES;"
+sudo mysql -sfu root "DELETE FROM mysql.user WHERE User='';"
+sudo mysql -sfu root "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+sudo mysql -sfu root "DROP DATABASE test;"
+sudo mysql -sfu root "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+sudo mysql -sfu root "FLUSH PRIVILEGES;"
 
 sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
 
-echo /etc/apache2/mods-enabled/dir.conf <  "<IfModule mod_dir.c>"
-echo /etc/apache2/mods-enabled/dir.conf << "<DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm"
-echo /etc/apache2/mods-enabled/dir.conf << "</IfModule>"
+sudo echo /etc/apache2/mods-enabled/dir.conf <  "<IfModule mod_dir.c>"
+sudo echo /etc/apache2/mods-enabled/dir.conf << "<DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm"
+sudo echo /etc/apache2/mods-enabled/dir.conf << "</IfModule>"
 
 sudo service apache2 restart
